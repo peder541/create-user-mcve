616c03210ac2060115210d40974c2ddfbe4bc021
Ben Pedersen <peder541@umn.edu>
2021-12-15 01:59:11 -0600

    Update firebase-admin to 10.0.0

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,27 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
+const { initializeApp } = require('firebase-admin/app');
+const { getAuth } = require('firebase-admin/auth');
+
+let app = initializeApp();
+let auth = getAuth();
 
 exports.reset = functions.https.onCall((data, context) => {
     let token = context.auth.token;
     let providerUid = token.firebase.identities['google.com'][0];
     
-    return admin.auth().deleteUser(context.auth.uid).then();
+    return auth.deleteUser(context.auth.uid).then();
 });
 
 exports.simulate = functions.https.onCall((data, context) => {
     let token = context.auth.token;
     let providerUid = token.firebase.identities['google.com'][0];
     
-    return admin.auth().deleteUser(context.auth.uid).then(() => {
-        return admin.auth().createUser({
+    return auth.deleteUser(context.auth.uid).then(() => {
+        return auth.createUser({
             displayName: token.name 
         })
         .then(user => {
-            return admin.auth().updateUser(user.uid, {
+            return auth.updateUser(user.uid, {
                 providerToLink: {
                     providerId: 'google.com',
                     uid: providerUid
